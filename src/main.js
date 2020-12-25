ab62b0ea07e7d8842ef14786b28b3849411ad4a6
郭治涛 <zhitao.guo@leweipai.cn>
2020-12-25 18:26:19 +0800

    feat: 安装lodash

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import _ from 'lodash'
 import router from './router'
 
 Vue.config.productionTip = false
+Vue.prototype._ = _
 
 /* eslint-disable no-new */
 new Vue({
